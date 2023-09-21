d5602743a2caaca8af2316054276788ae7a545fd
Jonas002 <143726873+Jonas002@users.noreply.github.com>
2023-09-21 14:28:44 -0300

    Update PrincipalFuncionario.java

--- PrincipalFuncionario.java
+++ PrincipalFuncionario.java
@@ -1,6 +1,16 @@
-public class PrincipalFuncionario {
-    public static void main(String [] args) {
-        Funcionario fu = new Funcionario(123, 80028922, 32, "Perla", "perola@gmail.com", 1000.90, "123.456.789-00");
+import java.util.Scanner;
+public class PrincipalFuncionario{
+public static void main(String [] args){
+        Scanner teclado = new Scanner(System.in);
+        int cod = teclado.nextInt();
+        int telefone = teclado.nextInt();
+        int idade = teclado.nextInt();teclado.nextLine();
+        String nome = teclado.nextLine();
+        String end = teclado.nextLine();
+        Double salario = teclado.nextDouble();teclado.nextLine();
+        String email = teclado.nextLine();
+
+        Funcionario fu = new Funcionario(cod, telefone, idade, nome, end, salario, email);
         System.out.println(fu.toString());
     }
 }
