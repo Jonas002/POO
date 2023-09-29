ee874bec48d733108ffb632e5a865903898a7d49
Jonas002 <143726873+Jonas002@users.noreply.github.com>
2023-09-29 12:09:39 -0300

    Update AulaVetor.java

--- AulaVetor.java
+++ AulaVetor.java
@@ -1,32 +1,35 @@
 import java.util.Scanner;
     public class AulaVetor{
         
-        /**
-         * @param args
-         */
         public static void main(String[]args){
             Scanner teclado = new Scanner(System.in);
-            System.out.println("Digite seu nome: ");
-            String nome = teclado.nextLine();
-            System.out.println("Digite sua idade: ");
-            int idade = teclado.nextInt();teclado.nextLine();
+            System.out.println("Digite a capacidade da agenda: ");
+            int capacidade = teclado.nextInt();teclado.nextLine();
+            Agenda agenda = new Agenda(capacidade);
+            String nome, telefone;
+            int idade;
 
-            Pessoa[] pessoas = new Pessoa[5];
-            pessoas[0] = new Pessoa(nome, idade);
+            for (int i = 0; i < capacidade; i++){
+                System.out.println("Digite seu nome: ");
+                nome = teclado.nextLine();
+                System.out.println("Digite sua idade: ");
+                idade = teclado.nextInt();teclado.nextLine();
+                System.out.println("Digite seu número: ");
+                telefone = teclado.nextLine();
+                
+                agenda.add(new Pessoa(nome, idade, telefone));
 
-            System.out.println("Digite seu nome: ");
-            nome = teclado.nextLine();
-            System.out.println("Digite sua idade: ");
-            idade = teclado.nextInt();teclado.nextLine();
-            pessoas[1] = new Pessoa(nome, idade);
-            for(int i = 0; i < pessoas.length; i++) {
-                if (pessoas[i] != null) {
-                    System.out.println(pessoas[i].getNome());
-
-                }
+                
+               
             }
 
+            System.out.println(agenda.getNomeMaisNova());
+            agenda.getPessoasAgenda();
+            
+           
 
-            System.out.println("Hello word");
+          
         }
+
+        
 }
