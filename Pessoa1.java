9ea6ba1d25735f69a494731d23266bc5099e84b7
Jonas002 <143726873+Jonas002@users.noreply.github.com>
2023-10-26 15:19:52 -0300

    Create Pessoa1.java

---
+++ Pessoa1.java
@@ -0,0 +1,42 @@
+public class Pessoa {
+    protected String nome;
+    int idade;
+    protected String telefone;
+
+    public Pessoa(String nome, int idade, String telefone){
+        this.nome = nome;
+        this.idade = idade;
+        this.telefone = telefone;
+
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public String getTelefone(){
+        return telefone;
+    }
+
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+
+
+    public String toString(){
+        return "Nome: " + nome + "\nIdade: " + idade + "\nTelefone:" + telefone;
+    }
+
+}
