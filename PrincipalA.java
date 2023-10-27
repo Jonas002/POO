0514962bc1143fdf8e31cf7a1ab9fb5d8d8862a9
Jonas002 <143726873+Jonas002@users.noreply.github.com>
2023-10-27 11:43:04 -0300

    Update PrincipalA.java

--- PrincipalA.java
+++ PrincipalA.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
 public class Principal {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        Cadastro cad = new Cadastro();
         System.out.print("Digite o nome do aluno: ");
         String nomealuno = teclado.nextLine();
         System.out.print("Digite a idade do aluno: ");
@@ -12,9 +16,12 @@ public class Principal {
         String mataluno = teclado.nextLine();
         int nota1 = teclado.nextInt();teclado.nextLine();
         int nota2 = teclado.nextInt();teclado.nextLine();
+        Aluno p5 = new Aluno(nomealuno, idadealuno, telefonealuno, mataluno, nota1, nota2);
         
         
-        Aluno p1 = new Aluno (nomealuno, idadealuno, telefonealuno, mataluno, nota1, nota2);
+        
+        Funcionario p2 = new Funcionario("Jonas", 16, "(83)099999", 30000);
+        Professor p3 = new Professor("Perla",17, "99999999999", 40000, "Matématica");
         /*System.out.print("Digite o nome do aluno: ");
         String nomealuno1 = teclado.nextLine();
         System.out.print("Digite a idade do aluno: ");
@@ -27,13 +34,21 @@ public class Principal {
         Funcionario p4 = new Professor("Peu", 18, "(83)981535148", 3000);
         Pessoa p5 = new Professor("Barros", 39, "(83)998405985", 3050);*/
 
-        System.out.println(p1.toString());
+        /*System.out.println(p5.toString());
+
+        p5.seViraNos30();
+        p2.seViraNos30();
+        p3.seViraNos30();*/
+      
+
+
         
         
 
 
-        /*cad.adicionar(p1);
-
-        cad.quadroSeViraNos30();*/ 
+        cad.adicionar(p5);
+        cad.adicionar(p2);
+        cad.adicionar(p3);
+        cad.quadroSeViraNos30();
     }
 }
