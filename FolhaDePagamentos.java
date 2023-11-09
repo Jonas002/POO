4502c35f88da54f164c7df07509501cf19fe69b9
Jonas002 <143726873+Jonas002@users.noreply.github.com>
2023-11-09 14:53:39 -0300

    Create FolhaDePagamentos.java

---
+++ FolhaDePagamentos.java
@@ -0,0 +1,13 @@
+public class FolhaDePagamento {
+    private Empregado[] Assaraliado;
+    private int qtdAssalariado = 0;
+    
+    //Construtor da Classe
+    public void adicionaAssaraiado(){
+        this.adicionaAssaraiado();
+    }
+
+    
+
+    
+}
