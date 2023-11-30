66a9f3037dd48c32115e2563676f909ca9293319
Jonas002 <143726873+Jonas002@users.noreply.github.com>
2023-11-30 14:28:26 -0300

    Update FreteExpresso.java

--- FreteExpresso.java
+++ FreteExpresso.java
@@ -1,9 +1,20 @@
-public class FreteExpresso {
-    private float distancia;
-    private float valorPorKm;
+public class FreteExpresso extends Frete implements Fretavel{
     private int nivelUrgencia;
     
+    public FreteExpresso(float distancia, float valorPorKm, int nivelUrgencia){
+        super(distancia, valorPorKm);
+        this.nivelUrgencia = nivelUrgencia;
+    }
+
+    public int getNivelUrgencia(){
+        return nivelUrgencia;
+    }
+
+    public void setNivelUrgencia(int nivelUrgencia){
+        this.nivelUrgencia = nivelUrgencia;
+    }
+
     public float calcularFrete(){
-        return (distancia * valorPorKm) + (nivelUrgencia * 100);
+        return (getDistancia() * getValorPorKm()) + (nivelUrgencia * 100);
     }
 }
