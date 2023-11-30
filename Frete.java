8e363949658f5ef85080cf8d927d4f75a3fab3e2
Jonas002 <143726873+Jonas002@users.noreply.github.com>
2023-11-30 14:27:44 -0300

    Update Frete.java

--- Frete.java
+++ Frete.java
@@ -1,4 +1,26 @@
 public abstract class Frete {
     private float distancia;
     private float valorPorKm; 
+
+    public Frete(float distancia, float valorPorKm){
+        this.distancia = distancia;
+        this.valorPorKm = valorPorKm;
+    }
+
+    public float getDistancia(){
+        return distancia;
+    }
+
+    public void setDistancia( float distancia){
+        this.distancia = distancia;
+    }
+
+    public float getValorPorKm(){
+        return valorPorKm;
+    }
+
+    public void setValorPorKm(float valorPorKm){
+        this.valorPorKm = valorPorKm;
+    }
+
 }
